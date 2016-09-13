cf3f4156e9084d532040285c5d85ad08097d9be9
Daniel Mårtensson <daniel.martensson89@outlook.com>
2016-09-13 22:57:18 +0200

    Start Projects

---
+++ TexasHoldEm HandCalculator/TexasHoldEm HandCalculator/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldEm_HandCalculator
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+        public static void Greeting()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
